55eb5119733ac380426015593d7debe78020edc0
Andrew <shitovandrey@list.ru>
2026-08-02 03:16:22 +0500

    return styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,9 @@
 import styles from './Card.module.scss';
-import React from 'react';
 
-function Card({ title, imageUrl, onFavorite, price, onPlus }) {
-  const [isAdded, setIsAdded] = React.useState(false);
-
-  const onClickPlus = () => {
-    onPlus({ title, imageUrl, price });
-    setIsAdded(!isAdded);
-  };
-
+function Card({ title, imageUrl, price }) {
   return (
     <div className={styles.card}>
-     {/* <div className={styles.favorite} onClick={onFavorite}>
+      {/* <div className={styles.favorite} onClick={onFavorite}>
         <img src="/img/heart-disabled.svg" alt="disabled" />
       </div>*/}
       <img src={imageUrl} className={styles.tovar_image} alt={title} />
@@ -20,12 +12,6 @@ function Card({ title, imageUrl, onFavorite, price, onPlus }) {
         <div>
           Цена {price} руб.
         </div>
-       <img
-          className={styles.plus}
-          onClick={onClickPlus}
-          src={isAdded ? '/img/btn-checked.svg' : '/img/btn-removed.svg'}
-          alt="Plus"
-        />
       </div>
     </div>
   );
